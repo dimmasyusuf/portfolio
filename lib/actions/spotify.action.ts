4bf6f33cafef746dbf824a3972d300ed8a7c4c25
dimmasyusuf <dimasyusufqurohman@gmail.com>
2024-01-11 00:17:46 +0700

    feat: create spotify action api

---
+++ lib/actions/spotify.action.ts
@@ -0,0 +1,77 @@
+import querystring from 'querystring';
+
+const client_id = process.env.SPOTIFY_CLIENT_ID;
+const client_secret = process.env.SPOTIFY_CLIENT_SECRET;
+const refresh_token = process.env.SPOTIFY_REFRESH_TOKEN;
+
+const TOKEN_ENDPOINT = 'https://accounts.spotify.com/api/token';
+const CURRENTLY_PLAYING_ENDPOINT =
+  'https://api.spotify.com/v1/me/player/currently-playing';
+
+export const getAccessToken = async (
+  client_id: string,
+  client_secret: string,
+  refresh_token: string
+) => {
+  const basic = Buffer.from(`${client_id}:${client_secret}`).toString('base64');
+
+  const response = await fetch(TOKEN_ENDPOINT, {
+    method: 'POST',
+    headers: {
+      Authorization: `Basic ${basic}`,
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    body: querystring.stringify({
+      grant_type: 'refresh_token',
+      refresh_token,
+    }),
+  });
+
+  return response.json();
+};
+
+export const getCurrentlyPlaying = async () => {
+  try {
+    const { access_token } = await getAccessToken(
+      client_id as string,
+      client_secret as string,
+      refresh_token as string
+    );
+
+    const response = await fetch(CURRENTLY_PLAYING_ENDPOINT, {
+      headers: {
+        Authorization: `Bearer ${access_token}`,
+      },
+    });
+
+    if (response.status >= 400) {
+      throw new Error('Unable to fetch currently playing');
+    } else if (response.status === 204) {
+      throw new Error('Currently not playing anything');
+    }
+
+    const song = await response.json();
+    const albumImageUrl = song.item.album.images[0].url;
+    const artistName = song.item.artists
+      .map((artist: any) => artist.name)
+      .join(', ');
+    const songUrl = song.item.external_urls.spotify;
+    const title = song.item.name;
+    const timePlayed = song.progress_ms;
+    const timeTotal = song.item.duration_ms;
+    const artistUrl = song.item.artists[0].external_urls.spotify;
+
+    return {
+      albumImageUrl,
+      artistName,
+      songUrl,
+      title,
+      timePlayed,
+      timeTotal,
+      artistUrl,
+    };
+  } catch (error) {
+    console.error(error);
+    throw new Error(typeof error === 'string' ? error : JSON.stringify(error));
+  }
+};
