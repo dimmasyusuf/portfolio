045484a86508a4ff7ce0f391a19ebe6d497c2103
dimmasyusuf <dimasyusufqurohman@gmail.com>
2024-01-11 17:50:05 +0700

    style: make text smaller and add link

--- components/Copyright.tsx
+++ components/Copyright.tsx
@@ -1,9 +1,21 @@
+import Link from 'next/link';
+
 export default function Copyright() {
   const year = new Date().getFullYear() as number;
 
   return (
     <div className="flex justify-center items-center p-4">
-      <p>&copy; {year} dimmasyusuf. All rights reserved.</p>
+      <p className="text-xs text-muted-foreground">
+        &copy; {year}{' '}
+        <Link
+          href="https://www.linkedin.com/in/dimmasyusuf/"
+          className="hover:text-primary"
+          target="_blank"
+        >
+          dimmasyusuf
+        </Link>
+        . All rights reserved.
+      </p>
     </div>
   );
 }
