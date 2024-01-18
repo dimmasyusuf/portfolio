85a331dd0c97cbdd2281658e3438492feb675dfb
dimmasyusuf <dimasyusufqurohman@gmail.com>
2024-01-19 00:28:39 +0700

    style: make education items look like card

--- components/EducationItem.tsx
+++ components/EducationItem.tsx
@@ -11,27 +11,20 @@ export default function EducationItem({
   logo,
 }: Education) {
   return (
-    <div className="flex gap-4">
+    <div className="flex flex-col gap-2 w-full p-6 border rounded-xl">
       <div className="relative flex justify-center items-center aspect-square rounded-md w-10 h-10 mt-1">
         <Image
           src={logo}
           alt={name}
-          fill
+          width={40}
+          height={40}
           className="rounded-md"
         />
       </div>
-      <div className="flex flex-col gap-2 w-full">
-        <div className="flex flex-col">
-          <h4 className="font-bold">{name}</h4>
-          <p className="text-sm">{major}</p>
-          <p className="text-sm text-muted-foreground">{`${startDate} - ${endDate}`}</p>
-        </div>
-        <div className="flex flex-col">
-          {grade && <p className="text-sm">{`Grade: ${grade}`}</p>}
-          {activities && (
-            <p className="text-sm">{`Activities and societies: ${activities}`}</p>
-          )}
-        </div>
+      <div className="flex flex-col">
+        <h4 className="font-bold">{name}</h4>
+        <p className="text-sm">{major}</p>
+        <p className="text-sm text-muted-foreground">{`${startDate} - ${endDate}`}</p>
       </div>
     </div>
   );
