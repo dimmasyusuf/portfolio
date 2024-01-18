71b8026f1542d7187d63d2c0834ac35346dff77e
dimmasyusuf <dimasyusufqurohman@gmail.com>
2024-01-19 00:58:28 +0700

    fix: move and export authOptions from next auth route

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -2,7 +2,7 @@
 
 import prisma from '@/lib/prismadb';
 import { getServerSession } from 'next-auth';
-import { authOptions } from '@/app/api/auth/[...nextauth]/route';
+import authOptions from '@/app/api/auth/[...nextauth]/options';
 import { handleError } from '../utils';
 import bcrypt from 'bcrypt';
 
