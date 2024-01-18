3454e24464193c516994c27d2c4750ab30b7f1a7
dimmasyusuf <dimasyusufqurohman@gmail.com>
2024-01-19 01:16:45 +0700

    fix: change export method authOptions at sign in and sign up page

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,6 +1,6 @@
 import SignUpForm from '@/components/SignUpForm';
 import { getServerSession } from 'next-auth';
-import { authOptions } from '@/app/api/auth/[...nextauth]/route';
+import { authOptions } from '@/app/api/auth/[...nextauth]/options';
 import { redirect } from 'next/navigation';
 
 export default async function SignUpPage() {
