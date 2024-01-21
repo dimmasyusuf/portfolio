db722510de318e7ec9b33e59fefa945c70a3d698
dimmasyusuf <dimasyusufqurohman@gmail.com>
2024-01-21 23:26:19 +0700

    fix: unpass loading data from child, and add user loading skeleton

--- components/GuestList.tsx
+++ components/GuestList.tsx
@@ -10,16 +10,17 @@ import { getUserProfile } from '@/lib/actions/user.action';
 import { getAllMessages } from '@/lib/actions/message.action';
 import { useQuery } from '@tanstack/react-query';
 import { useSession } from 'next-auth/react';
+import { Skeleton } from './ui/skeleton';
 
 export default function GuestList() {
   const { status } = useSession();
 
-  const { data: messages, isLoading } = useQuery({
+  const { data: messages, isLoading: messageLoading } = useQuery({
     queryKey: ['messages'],
     queryFn: () => getAllMessages(),
   });
 
-  const { data: user } = useQuery({
+  const { data: user, isLoading: userLoading } = useQuery({
     queryKey: ['user'],
     queryFn: () => getUserProfile(),
     enabled: status === 'authenticated',
@@ -29,15 +30,21 @@ export default function GuestList() {
     <section className="flex flex-col gap-6 w-full">
       <div className="flex items-center justify-between">
         <h2 className="font-bold text-2xl">Guestbook</h2>
-        {user ? (
-          <GuestProfile user={user} />
+        {userLoading ? (
+          <Skeleton className="w-10 h-10 aspect-square rounded-md" />
         ) : (
-          <Button
-            size="sm"
-            asChild
-          >
-            <Link href="/sign-in">Sign in</Link>
-          </Button>
+          <>
+            {user ? (
+              <GuestProfile user={user} />
+            ) : (
+              <Button
+                size="sm"
+                asChild
+              >
+                <Link href="/sign-in">Sign in</Link>
+              </Button>
+            )}
+          </>
         )}
       </div>
       <ScrollArea
@@ -55,14 +62,29 @@ export default function GuestList() {
               user ? 'min-h-[473px]' : 'min-h-[481px]'
             } flex flex-col gap-6`}
           >
-            {messages?.map((message) => (
-              <GuestItem
-                key={message.id}
-                message={message}
-                isLoading={isLoading}
-                user={user}
-              />
-            ))}
+            {messageLoading
+              ? Array.from({ length: 7 })?.map((_, index: number) => (
+                  <div
+                    key={index}
+                    className="flex justify-between gap-2 w-full items-center"
+                  >
+                    <Skeleton className="w-10 h-10 aspect-square rounded-md" />
+                    <div className="flex flex-col gap-1 w-full">
+                      <div className="flex justify-between items-center gap-2">
+                        <Skeleton className="w-4/12 h-4" />
+                        <Skeleton className="w-16 h-4" />
+                      </div>
+                      <Skeleton className="w-6/12 h-4" />
+                    </div>
+                  </div>
+                ))
+              : messages?.map((message) => (
+                  <GuestItem
+                    key={message.id}
+                    message={message}
+                    user={user}
+                  />
+                ))}
           </div>
           <GuestInput />
         </div>
