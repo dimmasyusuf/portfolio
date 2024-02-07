014368e6f497669b4eb8a33c2fb0ead54ced7caf
dimmasyusuf <dimasyusufqurohman@gmail.com>
2024-02-07 23:57:11 +0700

    feat: add handleUnitChange and handleUnitBlur on DonateForm

--- components/DonateForm.tsx
+++ components/DonateForm.tsx
@@ -1,10 +1,10 @@
 'use client';
 
-import { donateInputSchema } from '@/lib/validator';
-import { zodResolver } from '@hookform/resolvers/zod';
+import React, { useState } from 'react';
+import Image from 'next/image';
 import { useForm } from 'react-hook-form';
-import { z } from 'zod';
-
+import { zodResolver } from '@hookform/resolvers/zod';
+import { set, z } from 'zod';
 import {
   Form,
   FormControl,
@@ -14,8 +14,6 @@ import {
 } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
 import { Textarea } from './ui/textarea';
-import { DonateDialog } from './DonateDialog';
-import { useState } from 'react';
 import { Button } from './ui/button';
 import {
   ArrowLeftIcon,
@@ -23,7 +21,7 @@ import {
   MinusIcon,
   PlusIcon,
 } from '@radix-ui/react-icons';
-import Image from 'next/image';
+import { donateInputSchema } from '@/lib/validator';
 
 export default function DonateForm() {
   const [step, setStep] = useState(1);
@@ -42,8 +40,24 @@ export default function DonateForm() {
     console.log(values);
   }
 
+  const handleUnitChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    let value = e.target.value;
+    const unit = parseInt(value);
+
+    if (unit < 1) setTotalUnit(1);
+    else if (unit > 9999) setTotalUnit(9999);
+    else setTotalUnit(unit);
+  };
+
+  const handleUnitBlur = () => {
+    if (isNaN(totalUnit)) {
+      setTotalUnit(1);
+      setTotalPrice(5000);
+    }
+  };
+
   return (
-    <div className="flex flex-col rounded-md p-6 gap-6 bg-background dark:bg-accent border h-[569px] justify-between">
+    <div className="flex flex-col rounded-md p-6 gap-6 bg-background dark:bg-accent border h-full sm:h-[569px] justify-between">
       <div className="flex flex-col space-y-1.5 text-center sm:text-left">
         <h3 className="text-lg font-semibold leading-none tracking-tight">
           Donate
@@ -72,55 +86,65 @@ export default function DonateForm() {
 
           <div className="flex flex-col gap-6 items-center justify-center w-full">
             <span className="text-lg font-bold">
-              Rp {totalPrice * totalUnit}
+              Rp{' '}
+              {isNaN(totalUnit)
+                ? totalPrice.toLocaleString('id-ID')
+                : (totalUnit * totalPrice).toLocaleString('id-ID')}
             </span>
 
             <div className="flex gap-3 items-center">
               <Button
                 size="icon"
                 onClick={() => setTotalUnit(totalUnit - 1)}
+                disabled={totalUnit <= 1}
               >
                 <MinusIcon className="w-4 h-4" />
               </Button>
               <Input
-                type="number"
-                min="1"
-                max="9999"
-                inputMode="numeric"
-                pattern="[0-9]*"
-                className="w-16 h-10 remove-arrow shadow-none text-center"
-                value={totalUnit}
+                value={isNaN(totalUnit) ? '' : totalUnit}
+                onChange={handleUnitChange}
+                onBlur={handleUnitBlur}
+                className="w-16 h-10 shadow-none text-center dark:border-neutral-50"
               />
               <Button
                 size="icon"
                 onClick={() => setTotalUnit(totalUnit + 1)}
+                disabled={totalUnit >= 9999}
               >
                 <PlusIcon className="w-4 h-4" />
               </Button>
             </div>
 
-            <div className="grid grid-cols-4 gap-2 w-full">
+            <div className="grid grid-cols-2 sm:grid-cols-4 gap-2 w-full">
               <Button
+                size="sm"
                 variant="outline"
-                className="shadow-none"
+                className="shadow-none dark:border-neutral-50 dark:hover:bg-primary dark:hover:text-primary-foreground"
+                onClick={() => setTotalUnit(5)}
               >
                 5 Units
               </Button>
               <Button
+                size="sm"
                 variant="outline"
-                className="shadow-none"
+                className="shadow-none dark:border-neutral-50 dark:hover:bg-primary dark:hover:text-primary-foreground"
+                onClick={() => setTotalUnit(10)}
               >
                 10 Units
               </Button>
               <Button
+                size="sm"
                 variant="outline"
-                className="shadow-none"
+                className="shadow-none dark:border-neutral-50 dark:hover:bg-primary dark:hover:text-primary-foreground"
+                onClick={() => setTotalUnit(25)}
               >
                 25 Units
               </Button>
               <Button
+                size="sm"
                 variant="outline"
-                className="shadow-none"
+                className="shadow-none dark:border-neutral-50 dark:hover:bg-primary dark:hover:text-primary-foreground"
+                onClick={() => setTotalUnit(100)}
               >
                 100 Units
               </Button>
@@ -176,7 +200,9 @@ export default function DonateForm() {
       <div className="flex justify-between items-center w-full">
         {step > 1 && (
           <Button
-            variant="secondary"
+            size="sm"
+            variant="outline"
+            className="dark:border-neutral-50 dark:hover:bg-primary dark:hover:text-primary-foreground"
             onClick={() => setStep(step - 1)}
           >
             <ArrowLeftIcon className="mr-2 w-4 h-4" />
@@ -185,6 +211,7 @@ export default function DonateForm() {
         )}
         {step < 3 && (
           <Button
+            size="sm"
             onClick={() => setStep(step + 1)}
             className="ml-auto"
           >
@@ -192,7 +219,10 @@ export default function DonateForm() {
           </Button>
         )}
         {step === 3 && (
-          <Button onClick={() => setStep(1)}>
+          <Button
+            size="sm"
+            onClick={() => setStep(1)}
+          >
             Donate <ArrowRightIcon className="ml-2 w-4 h-4" />
           </Button>
         )}
