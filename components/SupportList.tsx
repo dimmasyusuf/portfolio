9ed4a0772f7662b9213c7e1116dc25d7da31ee0f
dimmasyusuf <dimasyusufqurohman@gmail.com>
2024-02-10 19:07:35 +0700

    style: create no supports found ui at support list

--- components/SupportList.tsx
+++ components/SupportList.tsx
@@ -14,12 +14,19 @@ export default function SupportList() {
   return (
     <ScrollArea className="h-[586px] sm:h-[569px] w-full border p-4 rounded-md bg-card dark:bg-accent">
       <div className="flex flex-col gap-4">
-        {supports?.map((support) => (
-          <SupportItem
-            key={support.id}
-            support={support}
-          />
-        ))}
+        {supports?.length === 0 || !supports ? (
+          <div className="flex flex-col items-center justify-center gap-2 h-[552px] sm:h-[535px]">
+            <span className="text-9xl">🥺</span>
+            <span className="text-lg">No supports found.</span>
+          </div>
+        ) : (
+          supports?.map((support) => (
+            <SupportItem
+              key={support.id}
+              support={support}
+            />
+          ))
+        )}
       </div>
     </ScrollArea>
   );
