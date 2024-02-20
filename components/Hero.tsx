f20e61d5319e4621897d03028303bb24382c5876
dimmasyusuf <dimasyusufqurohman@gmail.com>
2024-02-21 00:37:56 +0700

    style: change hero buttons content

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,23 +1,12 @@
-'use client';
-
 import Image from 'next/image';
-import { useState } from 'react';
 import { Button } from './ui/button';
 import Link from 'next/link';
-import {
-  RiChat3Fill,
-  RiChat3Line,
-  RiHeart3Fill,
-  RiHeart3Line,
-  RiMapPinLine,
-} from 'react-icons/ri';
+import { MdAlternateEmail } from 'react-icons/md';
+import { RiEyeLine, RiMapPinLine, RiVerifiedBadgeFill } from 'react-icons/ri';
 import { Separator } from './ui/separator';
-import { GitHubLogoIcon } from '@radix-ui/react-icons';
+import { EnvelopeClosedIcon, GitHubLogoIcon } from '@radix-ui/react-icons';
 
 export default function Hero() {
-  const [guestbookHover, setGuestbookHover] = useState(false);
-  const [supportHover, setSupportHover] = useState(false);
-
   return (
     <section className="grid grid-cols-1 border-b sm:border sm:rounded-md">
       <div className="hero-bg flex flex-col gap-4 h-32 sm:h-48 w-full rounded-t-md" />
@@ -40,48 +29,49 @@ export default function Hero() {
               size="icon"
               variant="outline"
               asChild
-              aria-label="Contact me"
-              onMouseOver={() => setGuestbookHover(true)}
-              onMouseLeave={() => setGuestbookHover(false)}
               className="shadow-none h-8 w-8"
             >
-              <Link href="/guestbook">
-                {guestbookHover ? (
-                  <RiChat3Fill className="w-4 h-4" />
-                ) : (
-                  <RiChat3Line className="w-4 h-4" />
-                )}
+              <Link
+                href="mailto:dimasyusufqurohman@gmail.com"
+                aria-label="Send me an email"
+              >
+                <EnvelopeClosedIcon className="w-4 h-4" />
               </Link>
             </Button>
             <Button
               size="icon"
               variant="outline"
               asChild
-              aria-label="Support me"
-              onMouseOver={() => setSupportHover(true)}
-              onMouseLeave={() => setSupportHover(false)}
               className="shadow-none h-8 w-8"
             >
-              <Link href="/support">
-                {supportHover ? (
-                  <RiHeart3Fill className="w-4 h-4" />
-                ) : (
-                  <RiHeart3Line className="w-4 h-4" />
-                )}
+              <Link
+                href="https://github.com/dimmasyusuf/"
+                target="_blank"
+                rel="noopener noreferrer"
+                aria-label="Follow me on GitHub"
+              >
+                <GitHubLogoIcon className="w-4 h-4" />
               </Link>
             </Button>
             <Button
               size="sm"
               asChild
             >
-              <Link href="/support">Hire Me</Link>
+              <Link
+                href="https://www.linkedin.com/in/dimmasyusuf/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Hire Me
+              </Link>
             </Button>
           </div>
         </div>
 
         <div className="flex flex-col">
-          <h1 className="text-lg sm:text-2xl font-bold">
+          <h1 className="flex gap-2 items-center text-lg sm:text-2xl font-bold">
             Dimas Yusuf Qurohman
+            <RiVerifiedBadgeFill className="w-4 h-4 text-sky-500" />
           </h1>
           <p className="text-md sm:text-lg">Software Engineer</p>
         </div>
@@ -104,8 +94,10 @@ export default function Hero() {
             rel="noopener noreferrer"
             className="flex items-center gap-2"
           >
-            <GitHubLogoIcon className="w-4 h-4" />
-            <span className="text-xs hover:underline">@dimmasyusuf</span>
+            <RiEyeLine className="w-4 h-4" />
+            <span className="text-xs hover:underline hover:text-bold">
+              1923 Visitors
+            </span>
           </Link>
         </div>
       </div>
