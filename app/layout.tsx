94ff7c28f68bcf7de00019f361e34ae20f5212aa
dimmasyusuf <dimasyusufqurohman@gmail.com>
2024-02-21 01:01:19 +0700

    feat: add vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata, Viewport } from 'next';
+import { Analytics } from '@vercel/analytics/react';
 import { Inter as FontSans } from 'next/font/google';
 import './globals.css';
 
@@ -73,6 +74,7 @@ export default function RootLayout({
       >
         <Providers>{children}</Providers>
         <Toaster />
+        <Analytics />
       </body>
     </html>
   );
