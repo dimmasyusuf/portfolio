f9169f50f30ad8bd3f76208cf027e9071112d2c8
dimmasyusuf <dimasyusufqurohman@gmail.com>
2024-02-21 19:47:08 +0700

    style: restyle education list

--- components/EducationList.tsx
+++ components/EducationList.tsx
@@ -6,9 +6,12 @@ export default function Education() {
   const educations = data.educations;
 
   return (
-    <section className="flex flex-col gap-6">
-      <h2 className="text-2xl font-bold">Education</h2>
-      <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+    <section className="flex flex-col bg-background border rounded-md">
+      <span className="flex justify-between items-center p-4 border-b">
+        <h2 className="text-lg sm:text-xl font-bold">Education</h2>
+      </span>
+
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 p-4">
         {educations.map((education: Educations) => (
           <EducationItem
             key={education.id}
